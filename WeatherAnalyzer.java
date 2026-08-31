7fd86a136ef2ac5f63b87378650a63138064bec6
ccsandstorm <ccsandstrom@dons.usfca.edu>
2026-08-30 22:04:56 -0700

    Update WeatherAnalyzer.java

--- WeatherAnalyzer.java
+++ WeatherAnalyzer.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class WeatherAnalyzer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FileNotFoundException {
         // Main program logic
+        FileInputStream fileByteStream = null;
+        Scanner inFS = null;
+        readCSV("weather_data.csv");
+
     }
 
     public static ___________ readCSV(String filename) {
         // Read and parse CSV file
+        fileByteStream = new FileInputStream(filename);
+        inFS = new Scanner(fileByteStream);
+        while(inFS.hasNext()) { //while there are elements within the scanner...
+
+        }
     }
 
     public static _______ extractNumericColumn(_____ data, int columnIndex) {
